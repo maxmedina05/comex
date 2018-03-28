184cd65c1009f322fade016423a68801f11bb9b5
Max Medina <maxmedina05@gmail.com>
2018-03-28 18:36:34 -0400

    fix node serving static file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static('react-app/build'));
 
 	const path = require('path');
 	app.get('*', (req, res) => {
